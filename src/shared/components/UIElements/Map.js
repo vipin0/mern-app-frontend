e341e2e3ba2639fc61601a9cf6f9131fb9a2e7e6
vipin0 <vipin6673@gmail.com>
2021-04-15 15:17:18 +0530

    some changes

--- src/shared/components/UIElements/Map.js
+++ src/shared/components/UIElements/Map.js
@@ -22,22 +22,22 @@ const Map = props => {
     new window.google.maps.Marker({position:center,map:map})
     },[center,zoom]);
 
-    for openlayer map
+    ////for openlayer map
 
-      useEffect(() => {
-        new window.ol.Map({
-          target: mapRef.current.id,
-          layers: [
-            new window.ol.layer.Tile({
-              source: new window.ol.source.OSM()
-            })
-          ],
-          view: new window.ol.View({
-            center: window.ol.proj.fromLonLat([center.lng, center.lat]),
-            zoom: zoom
-          })
-        });
-      }, [center, zoom]);
+      // useEffect(() => {
+      //   new window.ol.Map({
+      //     target: mapRef.current.id,
+      //     layers: [
+      //       new window.ol.layer.Tile({
+      //         source: new window.ol.source.OSM()
+      //       })
+      //     ],
+      //     view: new window.ol.View({
+      //       center: window.ol.proj.fromLonLat([center.lng, center.lat]),
+      //       zoom: zoom
+      //     })
+      //   });
+      // }, [center, zoom]);
 
 
       // mapboxgl map
