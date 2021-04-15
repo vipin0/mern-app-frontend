1e1af0aa820b0255208336a6eb2436b40bc3f4c3
vipin0 <vipin6673@gmail.com>
2021-04-15 15:10:37 +0530

    some chmages

--- src/shared/components/UIElements/Map.js
+++ src/shared/components/UIElements/Map.js
@@ -14,54 +14,54 @@ const Map = props => {
   // for foogle map
 
 
-    // useEffect(() => {
-    //     const map = new window.google.maps.Map(mapRef.current, {
-    //     center: center,
-    //     zoom:zoom
-    // });
-    // new window.google.maps.Marker({position:center,map:map})
-    // },[center,zoom]);
+    useEffect(() => {
+        const map = new window.google.maps.Map(mapRef.current, {
+        center: center,
+        zoom:zoom
+    });
+    new window.google.maps.Marker({position:center,map:map})
+    },[center,zoom]);
 
-    // for openlayer map
+    for openlayer map
 
-      // useEffect(() => {
-      //   new window.ol.Map({
-      //     target: mapRef.current.id,
-      //     layers: [
-      //       new window.ol.layer.Tile({
-      //         source: new window.ol.source.OSM()
-      //       })
-      //     ],
-      //     view: new window.ol.View({
-      //       center: window.ol.proj.fromLonLat([center.lng, center.lat]),
-      //       zoom: zoom
-      //     })
-      //   });
-      // }, [center, zoom]);
+      useEffect(() => {
+        new window.ol.Map({
+          target: mapRef.current.id,
+          layers: [
+            new window.ol.layer.Tile({
+              source: new window.ol.source.OSM()
+            })
+          ],
+          view: new window.ol.View({
+            center: window.ol.proj.fromLonLat([center.lng, center.lat]),
+            zoom: zoom
+          })
+        });
+      }, [center, zoom]);
 
 
       // mapboxgl map
       
-    useEffect(() => {
-        const map = new mapboxgl.Map({
-        container: mapRef.current.id,
-        style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
-        center: [center.lng, center.lat], // starting position [lng, lat]
-        zoom: zoom // starting zoom
-        });
-        var marker = new mapboxgl.Marker()
-        .setLngLat([center.lng, center.lat])
-        .addTo(map);
-        var nav = new mapboxgl.NavigationControl();
-        map.addControl(nav, 'top-left');
-        var scale = new mapboxgl.ScaleControl({
-        maxWidth: 80,
-        unit: 'imperial'
-        });
-        map.addControl(scale);
+    // useEffect(() => {
+    //     const map = new mapboxgl.Map({
+    //     container: mapRef.current.id,
+    //     style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
+    //     center: [center.lng, center.lat], // starting position [lng, lat]
+    //     zoom: zoom // starting zoom
+    //     });
+    //     var marker = new mapboxgl.Marker()
+    //     .setLngLat([center.lng, center.lat])
+    //     .addTo(map);
+    //     var nav = new mapboxgl.NavigationControl();
+    //     map.addControl(nav, 'top-left');
+    //     var scale = new mapboxgl.ScaleControl({
+    //     maxWidth: 80,
+    //     unit: 'imperial'
+    //     });
+    //     map.addControl(scale);
 
-        scale.setUnit('metric');
-    },[center,zoom]);
+    //     scale.setUnit('metric');
+    // },[center,zoom]);
 
 
   return (
